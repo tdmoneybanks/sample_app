c985cfa4fcb605a02c7fcb0b529a0e0c540fd0ff
tyler dunkel <tdunkel@iwu.edu>
2014-06-28 20:52:06 -0700

    editing user half done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,17 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+  def edit
+    @user = User.find(params[:id])
+  end
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      # Handle a successful update.
+    else
+      render 'edit'
+    end
+  end
   def create
     @user = User.new(user_params)
     if @user.save
